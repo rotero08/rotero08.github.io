48f7b20a759f362647b595cd9a05eb0c109d8690
Rafael Otero <roteroerazo2@gmail.com>
2023-05-01 21:55:05 -0500

    test 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,16 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function loadPage(url) {
+	fetch(url)
+	  .then(response => response.text())
+	  .then(html => {
+		history.pushState({html}, null, url);
+		document.documentElement.innerHTML = html;
+	  })
+	  .catch(error => console.error(error));
+  }
+
 (function($) {
 
 	var	$window = $(window),
